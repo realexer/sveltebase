126e275796f209d838169e9889bc5dcd7b5f67d6
Alexey Skrypnik <unrealvsrealer@gmail.com>
2021-03-21 12:32:21 +0200

    Support of regex groups in redirect rules.

--- src/redirects.js
+++ src/redirects.js
@@ -2,13 +2,18 @@ export const redirector = (req, res, next) =>
 {
 	const rules = [
 		[/^\/(en)\/?$/, '/'],
+		//[/^\/(?<lang>[a-z]{2})\/not_found?$/, '/{lang}/not_found_new'], // example
 	];
 
 	for(let [condition, redirect] of rules)
 	{
 		const match = req.path.match(condition);
-		if(match) {
-			const redirectUrl = req.path.replace(condition, redirect);
+		if (match) {
+			let redirectUrl = req.path.replace(condition, redirect);
+
+			for (let group in match.groups) {
+				redirectUrl = redirectUrl.replace(`{${group}}`, match.groups[group]);
+			}
 
 			res.redirect(301, redirectUrl);
 			return;
